492a5a73702c80a7045a3b9cdc801b73341613db
Will Tham <tham.will00@gmail.com>
2022-06-21 12:49:47 -0700

    removed button

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = (props) => (
         <SectionText>
         This web application shows the progression of the projects that I've completed as well as skills and achievements that I've obtained. I am always expanding my tool kit and experience with every project I try!
         </SectionText>
-        <Button onClick={props.handleClick}>Learn More</Button>
+
       </LeftSection>
     </Section>
   </>
